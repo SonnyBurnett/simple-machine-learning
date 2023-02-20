5a46ad20e3b99c980123b81431f7a98019cbc063
Taco Bakker <tsbakker65@gmail.com>
2023-02-20 12:09:32 +0100

    regression added as example

---
+++ regression.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+import data_prep
+
+df = data_prep.read_and_prepare_data()
+df_binary = df[['Calories', 'Fat']]
+
+print(df_binary.head())
+
+X = np.array(df_binary['Calories']).reshape(-1, 1)
+y = np.array(df_binary['Fat']).reshape(-1, 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+
+regr = LinearRegression()
+
+regr.fit(X_train, y_train)
+print(regr.score(X_test, y_test))
+
+y_pred = regr.predict(X_test)
+plt.scatter(X_test, y_test, color='b')
+plt.plot(X_test, y_pred, color='k')
+
+plt.show()
